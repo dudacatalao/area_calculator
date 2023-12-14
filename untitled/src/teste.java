601d26970a7f881ebdcd3e48a6e25689f54941ed
dudacatalao <dmelocatalao@gmail.com>
2023-12-14 16:04:22 -0300

    updates

--- untitled/src/teste.java
+++ untitled/src/teste.java
@@ -12,13 +12,14 @@ public class teste {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("CALCULING AREAS");
-        System.out.println("A- Trangle");
+        System.out.println("A- Triangle");
         System.out.println("B- Rectangle");
         System.out.println("C- Trapeze");
         System.out.println("D- Square");
         System.out.println("E- Diamond");
         System.out.println("F- Circle");
         System.out.println("G- Parallelogram");
+        System.out.println("H- Exit");
         System.out.println("What area do you want to calculate?");
         String option = sc.next();
 
@@ -28,15 +29,16 @@ public class teste {
                 double base = sc.nextDouble();
                 System.out.println("Insert the Height:");
                 double altura = sc.nextDouble();
-                System.out.println("Result: "+ triangle(base,altura));
+                System.out.println("Result: "+ Math.round(triangle(base,altura)));
                 break;
+
 
             case "B":
                 System.out.println("Insert the base:");
                 double base2 = sc.nextDouble();
                 System.out.println("Insert the Height:");
                 double altura2 = sc.nextDouble();
-                System.out.println("Result: " + rectangle(base2,altura2));
+                System.out.println("Result: " + Math.round(rectangle(base2,altura2)));
                 break;
 
             case "C":
@@ -46,13 +48,13 @@ public class teste {
                 double bigger_base = sc.nextDouble();
                 System.out.println("Insert the smaller base: ");
                 double smaller_base = sc.nextDouble();
-                System.out.println("Result: "+ trapeze(bigger_base,smaller_base,altura3));
+                System.out.println("Result: "+ Math.round(trapeze(bigger_base,smaller_base,altura3)));
                 break;
 
             case "D":
                 System.out.println("Insirt the value of the Side: ");
                 double side = sc.nextDouble();
-                System.out.println("Result: "+ square(side));
+                System.out.println("Result: "+ Math.round(square(side)));
                 break;
 
             case "E":
@@ -60,13 +62,13 @@ public class teste {
                 double larger_diagonal = sc.nextDouble();
                 System.out.println("Insert the value of the minor diagonal: ");
                 double minor_diagonal = sc.nextDouble();
-                System.out.println("Result: "+ Diamond(larger_diagonal,minor_diagonal));
+                System.out.println("Result: "+ Math.round(Diamond(larger_diagonal,minor_diagonal)));
                 break;
 
             case "F":
                 System.out.println("Insert the value of the Radius: ");
                 double r = sc.nextDouble();
-                System.out.println("Result: "+ Circle(r));
+                System.out.println("Result: "+ Math.round(Circle(r)));
                 break;
 
             case "G":
@@ -74,8 +76,11 @@ public class teste {
                 double base7 = sc.nextDouble();
                 System.out.println("Insert the Height:");
                 double altura7 = sc.nextDouble();
-                System.out.println("Result: " + Parallelogram(base7, altura7));
+                System.out.println("Result: " + Math.round(Parallelogram(base7, altura7)));
                 break;
+
+            case "H":
+                System.exit(0);
 
             default:
                 System.out.println("Invalid data");
